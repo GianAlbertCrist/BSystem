1b1ba1f303ab270f9d7ad4c60812a62b29059bda
Gian Daytic <s.daytic.gianalbert@cmu.edu.ph>
2025-03-21 21:17:36 +0800

    Added Database

--- Database/JSONDatabase.java
+++ Database/JSONDatabase.java
@@ -5,9 +5,14 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import Accounts.Account;
+import Bank.Bank;
+import Bank.BankLauncher;
+
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -53,5 +58,108 @@ public class JSONDatabase {
             // Log the error
             LOGGER.log(Level.SEVERE, "Error saving JSON file", e);
         }
+    }
+
+    public static JSONObject dataToDict(Object data) {
+        JSONObject jsonObject = new JSONObject();
+        if (data instanceof Bank bank) {
+            jsonObject.put("bankId", bank.getBankId());
+            jsonObject.put("bankName", bank.getName());
+            jsonObject.put("passcode", bank.getPasscode());
+            jsonObject.put("depositLimit", bank.getDepositLimit());
+            jsonObject.put("withdrawLimit", bank.getWithdrawLimit());
+            jsonObject.put("creditLimit", bank.getCreditLimit());
+            jsonObject.put("processingFee", bank.getProcessingFee());
+
+            JSONArray accountsArray = new JSONArray();
+            for (Account account : bank.getBankAccounts()) {
+                accountsArray.add(dataToDict(account));
+            }
+            jsonObject.put("accounts", accountsArray);
+
+        } else if (data instanceof Account account) {
+            jsonObject.put("accountNumber", account.getAccountNumber());
+            jsonObject.put("ownerFname", account.getOwnerFname());
+            jsonObject.put("ownerLname", account.getOwnerLname());
+            jsonObject.put("ownerEmail", account.getOwnerEmail());
+            jsonObject.put("pin", account.getPin());
+
+            JSONArray transactionsArray = new JSONArray();
+            for (Transaction transaction : account.getTransactions()) {
+                transactionsArray.add(dataToDict(transaction));
+            }
+            jsonObject.put("transactions", transactionsArray);
+            
+        } else if (data instanceof Transaction transaction) {
+            jsonObject.put("accountNum", transaction.getAccountNum());
+            jsonObject.put("type", transaction.getType().toString());
+            jsonObject.put("description", transaction.getDescription());
+        }
+        return jsonObject;
+    }
+
+    public static Object dataFromDict(JSONObject jsonObject, Class<?> clazz) {
+        if (clazz == Bank.class) {
+            int bankId = ((Long) jsonObject.get("bankId")).intValue();
+            String bankName = (String) jsonObject.get("bankName");
+            String passcode = (String) jsonObject.get("passcode");
+            double depositLimit = (Double) jsonObject.get("depositLimit");
+            double withdrawLimit = (Double) jsonObject.get("withdrawLimit");
+            double creditLimit = (Double) jsonObject.get("creditLimit");
+            double processingFee = (Double) jsonObject.get("processingFee");
+
+            Bank bank = new Bank(bankId, bankName, passcode, depositLimit, withdrawLimit, creditLimit, processingFee);
+
+            JSONArray accountsArray = (JSONArray) jsonObject.get("accounts");
+            for (Object obj : accountsArray) {
+                JSONObject accountObject = (JSONObject) obj;
+                Account account = (Account) dataFromDict(accountObject, Account.class);
+                bank.addNewAccount(account);
+            }
+            return bank;
+
+        } else if (clazz == Account.class) {
+            String accountNumber = (String) jsonObject.get("accountNumber");
+            String ownerFname = (String) jsonObject.get("ownerFname");
+            String ownerLname = (String) jsonObject.get("ownerLname");
+            String ownerEmail = (String) jsonObject.get("ownerEmail");
+            String pin = (String) jsonObject.get("pin");
+
+            Bank bank = BankLauncher.findAccount(accountNumber);
+            Account account = new Account(bank, accountNumber, pin, ownerFname, ownerLname, ownerEmail) {};
+
+            JSONArray transactionsArray = (JSONArray) jsonObject.get("transactions");
+            for (Object obj : transactionsArray) {
+                JSONObject transactionObject = (JSONObject) obj;
+                Transaction transaction = (Transaction) dataFromDict(transactionObject, Transaction.class);
+                account.addNewTransaction(transaction.getAccountNum(), transaction.getType(), transaction.getDescription());
+            }
+            return account;
+
+        } else if (clazz == Transaction.class) {
+            String accountNum = (String) jsonObject.get("accountNum");
+            Transaction.Transactions type = Transaction.Transactions.valueOf((String) jsonObject.get("type"));
+            String description = (String) jsonObject.get("description");
+            return new Transaction(accountNum, type, description);
+        }
+        return null;
+    }
+
+    public static ArrayList<Object> loadData(String filename, Class<?> clazz) {
+        JSONArray jsonArray = load(filename);
+        ArrayList<Object> dataList = new ArrayList<>();
+        for (Object obj : jsonArray) {
+            JSONObject jsonObject = (JSONObject) obj;
+            dataList.add(dataFromDict(jsonObject, clazz));
+        }
+        return dataList;
+    }
+
+    public static void saveData(ArrayList<Object> dataList, String filename) {
+        JSONArray jsonArray = new JSONArray();
+        for (Object data : dataList) {
+            jsonArray.add(dataToDict(data));
+        }
+        save(jsonArray, filename);
     }
 }
